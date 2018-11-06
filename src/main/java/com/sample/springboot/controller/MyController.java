d9150614276477855fc494dbd2f8d0e2d94f5e44
nirmal <lbnnirmal@yahoo.com>
2018-11-06 15:39:26 +0800

    Change

--- src/main/java/com/sample/springboot/controller/MyController.java
+++ src/main/java/com/sample/springboot/controller/MyController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import com.sample.springboot.dao.CountryDao;
 import com.sample.springboot.dao.model.Country;
 
-
 @Controller
 public class MyController {
 	
